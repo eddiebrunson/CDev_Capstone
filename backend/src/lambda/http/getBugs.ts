ef5c2ad1601aa8b446f05b00b229ffc6c7fdaafa
Edward Brunson <eddiebrunson@gmail.com>
2020-09-24 02:07:50 -0400

    Update file name

---
+++ backend/src/lambda/http/getBugs.ts
@@ -0,0 +1,36 @@
+import 'source-map-support/register'
+import { getBugs } from '../../businessLogic/bugs'
+import { createLogger } from '../../utils/logger'
+import { getToken } from '../../helpers/authHelper'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
+const logger = createLogger('getBugs')
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  // TODO: Get all BUG items for a current user
+  logger.info('Processing GetBugs Event: ', event)
+  try{
+  const jwtToken: string = getToken(event.headers.Authorization)
+  const todoItems = await getBugs(jwtToken)
+  logger.info('Get bugs successful')
+  console.log(todoItems)
+
+
+  return {
+    statusCode: 200, 
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      items: todoItems,
+    })
+  }
+
+} catch (error) {
+  if(error.statusCode == 403) {
+    console.error('The bug does not have an image yet.')
+   }
+
+  }
+}
