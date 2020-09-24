63071b2d1f4ebc3f963c95f89572a0b3b814a768
Edward Brunson <eddiebrunson@gmail.com>
2020-09-24 01:53:05 -0400

    Update businessLogic to bugs

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,13 +1,13 @@
 import 'source-map-support/register'
 import { createLogger } from '../../utils/logger'
-import { deleteTodo } from '../../businessLogic/todos'
+import { deleteTodo } from '../../businessLogic/bugs'
 
 const logger = createLogger('deleteTodo')
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Remove a TODO item by id  
+  // TODO: Remove a BUG item by id  
   const todoId = event.pathParameters.todoId;
   const authorization = event.headers.Authorization;
   const split = authorization.split(' ');
